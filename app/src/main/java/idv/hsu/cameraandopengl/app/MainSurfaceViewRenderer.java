b304d224b8089c56b381e020f61349d5127ce3a0
HsuHsiaoHsuan <hsuhsiaohsuan@gmail.com>
2015-06-15 10:40:15 +0800

    [2015/06/15]     1. AirHock v2

--- app/src/main/java/idv/hsu/cameraandopengl/app/MainSurfaceViewRenderer.java
+++ app/src/main/java/idv/hsu/cameraandopengl/app/MainSurfaceViewRenderer.java
@@ -1,104 +1,72 @@
 package idv.hsu.cameraandopengl.app;
 
 import android.content.Context;
-import android.content.res.Resources;
 import android.opengl.GLSurfaceView;
 import idv.hsu.cameraandopengl.app.utils.ShaderHelper;
 import idv.hsu.cameraandopengl.app.utils.TextResourceReader;
 
-import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
-import static android.opengl.GLES20.glClear;
-import static android.opengl.GLES20.glClearColor;
-import static android.opengl.GLES20.glViewport;
-
-import static android.opengl.GLES20.*;
-import static android.opengl.GLUtils.*;
-import static android.opengl.Matrix.*;
-
 import javax.microedition.khronos.egl.EGLConfig;
 import javax.microedition.khronos.opengles.GL10;
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.nio.ByteBuffer;
 import java.nio.ByteOrder;
 import java.nio.FloatBuffer;
 
+import static android.opengl.GLES20.*;
+
 /**
  * Created by freeman on 2015/6/10.
  */
-public class MainSurfaceViewRenderer implements GLSurfaceView.Renderer/*, GLSurfaceView.EGLContextFactory*/ {
+public class MainSurfaceViewRenderer implements GLSurfaceView.Renderer {
     private static final String TAG = MainSurfaceViewRenderer.class.getSimpleName();
     private static final boolean D = false;
 
-    private Context mContext;
-    private static final int POSITION_COMPONENT_COUNT = 2; // ues two floating point values per vertex, so ...2
-    private float[] tableVertices = {
-            0f,  0f,
-            0f, 14f,
-            9f, 14f,
-            9f,  0f
-    };
-    private float[] tableVerticesWithTriangles = {
-            // first triangle
-            -0.5f, -0.5f, //0f,  0f,
-             0.5f,  0.5f, // 9f, 14f,
-            -0.5f,  0.5f, // 0f, 14f,
-
-            // second triangle
-            -0.5f, -0.5f, // 0f,  0f,
-             0.5f, -0.5f, // 9f,  0f,
-             0.5f,  0.5f,// 9f, 14f,
-
-            // line
-            -0.5f, 0f, // 0f,  7f,
-             0.5f, 0f, // 9f,  7f,
-
-            // malets
-            0f, -0.25f, // 4.5f,2f,
-            0f,  0.25f  // 4.5f,12f
-    };
-    private static final int BYTES_PER_FLOAT = 4;
-    private final FloatBuffer vertexData;
-    private int program;
-
-    private static final String U_COLOR = "u_Color";
-    private int uColorLocation;
     private static final String A_POSITION = "a_Position";
-    private int aPositionLocation;
+    private static final String A_COLOR = "a_Color";
+    private static final int POSITION_COMPONENT_COUNT = 2; // ues two floating point values per vertex, so ...2
+    private static final int COLOR_COMPONENT_COUNT = 3;
+    private static final int BYTES_PER_FLOAT = 4;
+    // 2 position code + 3 color code
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
 
+    private final FloatBuffer vertexData;
+    private final Context mContext;
+
+    private int program;
+    private int aPositionLocation;
+    private int aColorLocation;
     private GLSurfaceView mSurfaceView;
 
     public MainSurfaceViewRenderer(Context context) {
         mContext = context;
         mSurfaceView = new GLSurfaceView(context);
         mSurfaceView.setEGLContextClientVersion(2);
-//        mSurfaceView.setEGLContextFactory(this);
-//        mSurfaceView.setEGLConfigChooser(new ConfigChooser(5, 6, 5, 0, 16, 0));
-        mSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 0);
+//        mSurfaceView.setEGLConfigChooser(5, 6, 5, 0, 16, 0);
+        mSurfaceView.setEGLConfigChooser(8, 8, 8, 8, 16, 0);
         mSurfaceView.setRenderer(this);
 
+        float[] tableVerticesWithTriangles = {
+                // Order of coordinates: X, Y, R, G, B
+                   0f,    0f,   1f,   1f,   1f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                 0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                 0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+
+                // line
+                -0.5f, 0f, 1f, 0f, 0f,
+                 0.5f, 0f, 1f, 0f, 0f,
+
+                // malets
+                0f, -0.25f, 0f, 0f, 1f,
+                0f,  0.25f, 1f, 0f, 0f
+        };
+
         vertexData = ByteBuffer.allocateDirect(tableVerticesWithTriangles.length * BYTES_PER_FLOAT)
-                               .order(ByteOrder.nativeOrder())
-                               .asFloatBuffer();
+                .order(ByteOrder.nativeOrder())
+                .asFloatBuffer();
         vertexData.put(tableVerticesWithTriangles);
     }
-//
-//    @Override
-//    public EGLContext createContext(EGL10 egl, EGLDisplay display, EGLConfig eglConfig) {
-//        final int EGL_CONTEXT_CLIENT_VERSION = 0x3098;
-//        int[] attr = {
-//            EGL_CONTEXT_CLIENT_VERSION, 2, EGL10.EGL_NONE
-//        };
-//
-//        return egl.eglCreateContext(display, eglConfig,EGL10.EGL_NO_CONTEXT, attr);
-//    }
-//
-//    @Override
-//    public void destroyContext(EGL10 egl, EGLDisplay display, EGLContext context) {
-//        egl.eglDestroyContext(display, context);
-//    }
 
     @Override
     public void onSurfaceCreated(GL10 gl10, EGLConfig eglConfig) {
@@ -107,22 +75,29 @@ public class MainSurfaceViewRenderer implements GLSurfaceView.Renderer/*, GLSurf
         String vertexShaderSource = TextResourceReader.readTextFromResource(mContext, R.raw.simple_vertex_shader);
         String fragmentShaderSource = TextResourceReader.readTextFromResource(mContext, R.raw.simple_fragment_shader);
 
-        int vertexShader = ShaderHelper.compileVertexShader(vertexShaderSource);
+        int vertexShader   = ShaderHelper.compileVertexShader(vertexShaderSource);
         int fragmentShader = ShaderHelper.compileFragmentShader(fragmentShaderSource);
+
         program = ShaderHelper.linkProgram(vertexShader, fragmentShader);
         if (D) {
             ShaderHelper.validateProgram(program);
         }
         glUseProgram(program);
 
-        uColorLocation = glGetUniformLocation(program, U_COLOR); // find simple_vertex_shader.glsl
         aPositionLocation = glGetAttribLocation(program, A_POSITION); // find simple_gragment_shader.glsl
+        aColorLocation    = glGetAttribLocation(program, A_COLOR);    // find simple_vertex_shader.glsl
 
         vertexData.position(0);
         // tell OpenGL find data for a_Position in the buffer vertexData.
         // MUST VERY CAREFUL!
-        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, 0, vertexData);
+        glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+
         glEnableVertexAttribArray(aPositionLocation);
+
+        vertexData.position(POSITION_COMPONENT_COUNT);
+        glVertexAttribPointer(aColorLocation, COLOR_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
+
+        glEnableVertexAttribArray(aColorLocation);
     }
 
     @Override
@@ -133,89 +108,17 @@ public class MainSurfaceViewRenderer implements GLSurfaceView.Renderer/*, GLSurf
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
-        glUniform4f(uColorLocation, 1.0f, 1.0f, 1.0f, 1.0f); // update value of su_Color
-        glDrawArrays(GL_TRIANGLES, 0, 6); // start from 0, and read in six vertices.
 
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
+        glDrawArrays(GL_TRIANGLE_FAN, 0, 6); // start from 0, and read in six vertices
+
         glDrawArrays(GL_LINES, 6, 2);
 
-        glUniform4f(uColorLocation, 0.0f, 0.0f, 1.0f, 1.0f);
         glDrawArrays(GL_POINTS, 8, 1);
 
-        glUniform4f(uColorLocation, 1.0f, 0.0f, 0.0f, 1.0f);
         glDrawArrays(GL_POINTS, 9, 1);
     }
 
     public GLSurfaceView getGLSurfaceView() {
         return mSurfaceView;
     }
-
-//    private static class ConfigChooser implements GLSurfaceView.EGLConfigChooser {
-//        protected int mRedSize;
-//        protected int mGreenSize;
-//        protected int mBlueSize;
-//        protected int mAlphaSize;
-//        protected int mDepthSize;
-//        protected int mStencilSize;
-//        private int[] mValue = new int[1];
-//
-//        public ConfigChooser(int r, int g, int b, int a, int depth, int stencil) {
-//            mRedSize = r;
-//            mGreenSize = g;
-//            mBlueSize = b;
-//            mAlphaSize = a;
-//            mDepthSize = depth;
-//            mStencilSize = stencil;
-//        }
-//
-//        private int findConfigAttrib(EGL10 egl, EGLDisplay display, EGLConfig config, int attribute, int defaultValue) {
-//            if (egl.eglGetConfigAttrib(display, config, attribute, mValue)) {
-//                return mValue[0];
-//            }
-//
-//            return defaultValue;
-//        }
-//
-//        private EGLConfig getMatchingConfig(EGL10 egl, EGLDisplay display, int[] configAttribs) {
-//            int[] num_config = new int[1];
-//            egl.eglChooseConfig(display, configAttribs, null, 0, num_config);
-//
-//            int numConfigs = num_config[0];
-//            if (numConfigs <= 0) {
-//                throw new IllegalArgumentException("No matching EGL configs");
-//            }
-//
-//            EGLConfig[] configs = new EGLConfig[numConfigs];
-//            egl.eglChooseConfig(display, configAttribs, configs, numConfigs, num_config);
-//
-//            return chooseConfig(egl, display, configs);
-//        }
-//
-//        @Override
-//        public EGLConfig chooseConfig(EGL10 egl10, EGLDisplay eglDisplay) {
-//            return null;
-//        }
-//
-//        public EGLConfig chooseConfig(EGL10 egl, EGLDisplay display, EGLConfig[] configs) {
-//            for (EGLConfig config : configs) {
-//                int d = findConfigAttrib(egl, display, config, EGL10.EGL_DEPTH_SIZE, 0);
-//                int s = findConfigAttrib(egl, display, config, EGL10.EGL_STENCIL_SIZE, 0);
-//
-//                if (d < mDepthSize || s < mStencilSize) {
-//                    continue;
-//                }
-//
-//                int r = findConfigAttrib(egl, display, config, EGL10.EGL_RED_SIZE, 0);
-//                int g = findConfigAttrib(egl, display, config, EGL10.EGL_GREEN_SIZE, 0);
-//                int b = findConfigAttrib(egl, display, config, EGL10.EGL_BLUE_SIZE, 0);
-//                int a = findConfigAttrib(egl, display, config, EGL10.EGL_ALPHA_SIZE, 0);
-//
-//                if (r == mRedSize && g == mGreenSize && b == mBlueSize && a == mAlphaSize) {
-//                    return config;
-//                }
-//            }
-//            return  null;
-//        }
-//    }
-
 }
