2d3acfbc083b7e61de9feed7ea21f3bd60c2eee1
freeman <freeman@zhenhai.com.tw>
2015-07-07 17:18:20 +0800

    [2015/07/07] 1. page 120

--- app/src/main/java/idv/hsu/cameraandopengl/app/utils/TextureHelper.java
+++ app/src/main/java/idv/hsu/cameraandopengl/app/utils/TextureHelper.java
@@ -1,9 +1,11 @@
 package idv.hsu.cameraandopengl.app.utils;
 
 import android.content.Context;
+import android.graphics.Bitmap;
+import android.graphics.BitmapFactory;
 import android.util.Log;
 
-import static android.opengl.GLES20.glGenTextures;
+import static android.opengl.GLES20.*;
 
 /**
  * Created by freeman on 2015/6/26.
@@ -20,6 +22,26 @@ public class TextureHelper {
             if (D) {
                 Log.w(TAG, "Could not generate a new OpenGL texture object.");
             }
+            return 0;
         }
+
+        final BitmapFactory.Options options = new BitmapFactory.Options();
+        options.inScaled = false;
+
+        final Bitmap bitmap = BitmapFactory.decodeResource(
+                context.getResources(),
+                resourceId, options);
+
+        if (bitmap == null) {
+            if (D) {
+                Log.w(TAG, "Resource ID " + resourceId + " could not be decoded.");
+            }
+
+            glDeleteTextures(1, textureObjectIds, 0);
+        }
+
+        glBindTexture(GL_TEXTURE_2D, textureObjectIds[0]);
+
+        return 0;
     }
 }
